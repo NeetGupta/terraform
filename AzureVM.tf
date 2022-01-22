094da65bd22aa5ed943abf0f7492d308dd34be04
Neetu Gupta <guptaneetu703@gmail.com>
2022-01-22 18:50:44 +0530

    AzureVM.tf

--- AzureVM.tf
+++ AzureVM.tf
@@ -12,3 +12,15 @@ resource "Terraform-Resource" "{ResourceGroup}" {
   name = "{ResourceGroup}"
   loation = "eastus"
 }
+resource "azurerm_virtual_network" "{VirtualNetwork}" {
+  name                = "{VirtualNetwork}"
+  address_space       = ["10.1.0.0/24"]
+  location            = azurerm_resource_group.{ResourceGroup}.location
+  resource_group_name = azurerm_resource_group.{ResourceGroup}.name
+}
+
+resource "azurerm_subnet" "{Subnet}" {
+  name                 = "{Subnet}"
+  resource_group_name  = azurerm_resource_group.{ResourceGroup}.name
+  virtual_network_name = azurerm_virtual_network.{VirtualNetwork}.name
+  address_prefixes     = ["10.5.0.0/26"]
